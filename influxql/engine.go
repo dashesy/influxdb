50590c4620f955cdaf807958ef5dd5fb349fbbad
Paul Dix <paul@pauldix.net>
2015-02-08 06:06:30 -0500

    WIP: wire up continuous queries
    * Fix run to work with CQ broker
    * Fix CQ handler
    * Fix SetTimeRange to use RFC3339Nano
    * Fix the time range tests
    * Fix the parser to parse for RFC3339Nano literals in addition to the other format
    * Add logic for running CQs
    * Remove duplicate WriteBufferSize default setting from config

--- influxql/engine.go
+++ influxql/engine.go
@@ -314,7 +314,7 @@ loop:
 	for _, row := range rows {
 		for _, values := range row.Values {
 			t := time.Unix(0, values[0].(int64))
-			values[0] = t.UTC().Format(time.RFC3339Nano)
+			values[0] = t.UTC()
 		}
 		a = append(a, row)
 	}
@@ -651,6 +651,7 @@ func MapMean(itr Iterator, e *Emitter, tmin int64) {
 	out := &meanMapOutput{}
 
 	for k, v := itr.Next(); k != 0; k, v = itr.Next() {
+		warn("> ", k, v, itr.Tags())
 		out.Count++
 		out.Sum += v.(float64)
 	}
